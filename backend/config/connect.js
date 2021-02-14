ebb6135c2a26dab937bc30b1ffb8e7f0fba8807f
Eduardo Avalos <eduardoavalospalacios@gmail.com>
2021-02-14 21:55:42 +0100

    Database connection updated

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -2,12 +2,50 @@ require('dotenv').config();
 const mysql = require('mysql');
 const db = require('./db.js');
 
+const pool = mysql.createPool(db.db);
+
+let connection = (function () {
+    console.log('Conexión a la BD correcta');
+    
+    function _query(query, params, callback) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                connection.release();
+                callback(null, err);
+                throw err;
+            }
+
+            connection.query(query, params, function (err, rows) {
+                connection.release();
+                if (!err) {
+                    callback(rows);
+                }
+                else {
+                    callback(null, err);
+                }
+
+            });
+
+            connection.on('error', function (err) {
+                connection.release();
+                callback(null, err);
+                throw err;
+            });
+        });
+    };
+
+    return {
+        query: _query
+    };
+})();
+
+/*
 const connection = mysql.createConnection(db.db);
 
 connection.connect((err, conn) => {
     if (err) throw err;
     console.log('Conexión a la BD correcta');
 });
-
+*/
 
 module.exports = connection;
